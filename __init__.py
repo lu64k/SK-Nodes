de851c605ee41e5249588a7ce436e887e631827a
lu64k <134982769+lu64k@users.noreply.github.com>
2024-10-03 11:12:46 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,19 +1,23 @@
-from .sikai_node import imageselect, LoadAnyLLM, GenerateTextFromLLM, OpenAIDAlleNode, SK_Text_String, SK_Random_File_Name
+from .sikai_node import imageselect, LoadAnyLLM, GenerateTextFromLLM, OpenAIDAlleNode, LoadNemotron, SK_Text_String, SK_Random_File_Name, OpenAI_Text_Node, ToneLayerQuantize, ColorTransferToneLayer, ImageTracingNode, NaturalSaturationAdjust, greyscaleblendNode
 
 NODE_CLASS_MAPPINGS = {
-    "Image Select": imageselect,
-    "Load LLM": LoadAnyLLM,
-    "Ask LLM": GenerateTextFromLLM,
     "SK Text_String": SK_Text_String,
-    "OpenAI DAlle Node": OpenAIDAlleNode,
-    "SK Random File Name": SK_Random_File_Name
+    "OpenAI Text Node": OpenAI_Text_Node,
+    "SK Random File Name": SK_Random_File_Name,
+    "Tone Layer Quantize": ToneLayerQuantize,
+    "Image Tracing Node": ImageTracingNode,
+    "Natural Saturation": NaturalSaturationAdjust,
+    "grey_scale blend": greyscaleblendNode,
+    "Load_Nemotron": LoadNemotron
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Image Select": "image select",
-    "Load LLM": "Load AnyLLM",
-    "Ask LLM": "Ask LLM",
-    "OpenAI DAlle Node": "OpenAI DAlle Node",
+    "OpenAI Text Node": "OpenAI Text Node",
     "SK Text_String": "SK Text_String",
-    "SK Random File Name": "SK Random File Name"
+    "SK Random File Name": "SK Random File Name",
+    "Tone Layer Quantize": "Tone Layer Quantize",
+    "Natural Saturation": "Natural Saturation",
+    "Image Tracing Node": "Image Tracing Node",
+    "grey_scale blend": "grey_scale blend",
+    "Load_Nemotron": "Load_Nemotron"
 }
 __all__ = ['NODE_CLASS_MAPPINGS']
